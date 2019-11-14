b4117b5455e65cf91482f3b3fb304f6889ca319c
vigli-Lee <wnsqjadl@gmail.com>
2019-11-14 22:07:36 +0900

    [func] 주사위 번호를 주사위 이미지로 변경

--- app/src/main/java/mobile/vigli/com/diceroller/MainActivity.kt
+++ app/src/main/java/mobile/vigli/com/diceroller/MainActivity.kt
@@ -3,23 +3,32 @@ package mobile.vigli.com.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 import org.w3c.dom.Text
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var diceTextView: TextView
+    private lateinit var diceImageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-         diceTextView = findViewById(R.id.diceTextView)
+         diceImageView = findViewById(R.id.diceImageView)
     }
 
     fun onClick(v: View) {
         val diceNumber = Random.nextInt(6) + 1
-        diceTextView.text = diceNumber.toString()
+
+        when (diceNumber) {
+            1 -> diceImageView.setImageResource(R.drawable.dice_1)
+            2 -> diceImageView.setImageResource(R.drawable.dice_2)
+            3 -> diceImageView.setImageResource(R.drawable.dice_3)
+            4 -> diceImageView.setImageResource(R.drawable.dice_4)
+            5 -> diceImageView.setImageResource(R.drawable.dice_5)
+            6 -> diceImageView.setImageResource(R.drawable.dice_6)
+        }
     }
 }
